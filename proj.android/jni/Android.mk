50f7c3ae7148c5d2e6778cf4cfae24cd71ad8161
Ricardo Quesada <ricardoquesada@gmail.com>
2015-10-08 16:39:22 -0700

    compiles on android

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,9 +10,11 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
+LOCAL_SRC_FILES := game/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/GameNode.cpp \
+                   ../../Classes/MainMenuNode.cpp \
+                   ../../Classes/Map.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
